bf7ccd6c6362c3fd4ac466bd5217e4a342d08cc3
张健  ZJ <zhangjian@chinamobo.com>
2016-12-08 18:12:29 +0800

    添加初始化文件

---
+++ discussionCircle/src/main/java/com/forlove/discussionCircle/system/ContextLoaderListener.java
@@ -0,0 +1,35 @@
+package com.forlove.discussionCircle.system;
+
+import com.forlove.discussionCircle.util.ApplicationContextUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.context.support.WebApplicationContextUtils;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+/**
+ * Created by Winter on 2016/12/8.
+ */
+public class ContextLoaderListener implements ServletContextListener {
+
+    private static Logger logger = LoggerFactory.getLogger(ContextLoaderListener.class);
+    private static XmlWebApplicationContext applicationContext;
+
+    @java.lang.Override
+    public void contextInitialized(ServletContextEvent sce) {
+        ServletContext context = sce.getServletContext();
+        applicationContext = (XmlWebApplicationContext) WebApplicationContextUtils.getWebApplicationContext(context);
+        ApplicationContextUtils.init(applicationContext);
+        context.setAttribute("startupDate", System.currentTimeMillis());
+        logger.info("初始化数据...");
+//        applicationContext.getBean(Contextinitse)
+    }
+
+    @java.lang.Override
+    public void contextDestroyed(ServletContextEvent sce) {
+
+    }
+}
